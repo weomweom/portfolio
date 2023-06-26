428d48acb9ee392014fa2d4097302f3a419f1d5c
weomweom <16.homiaq.16@gmail.com>
2023-06-26 21:53:52 +0200

    upd

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,17 +1,18 @@
 import "./style.css";
+import cv from "./CV2.pdf"
 
 const Header = () => {
     return ( 
         <header className="header">
             <div className="header__wrapper">
                 <h1 className="header__title">
-                    <strong>Hi, my name is <em>Katsiaryna</em></strong><br/>
+                    <strong>Hi, my name is <em>Kate</em></strong><br/>
                     a frontend developer
                 </h1>
                 <div className="header__text">
                     <p>with passion for learning and creating.</p>
                 </div>
-                <a href="CV.pdf" className="btn" download>Download CV</a>
+                <a href={cv} className="btn" download="cv.pdf">Download CV</a>
             </div>
         </header>
      );
--- src/helpers/projectsList.js
+++ src/helpers/projectsList.js
@@ -4,6 +4,8 @@ import projects02 from "./../img/projects/02.jpg";
 import projects02Big from "./../img/projects/02-big.jpg";
 import projects03 from "./../img/projects/03.jpg";
 import projects03Big from "./../img/projects/03-big.jpg";
+import projects04 from "./../img/projects/04.jpg";
+import projects04Big from "./../img/projects/04-big.jpg";
 
 const projects = [
     {
@@ -26,6 +28,13 @@ const projects = [
         img: projects03,
         imgBig: projects03Big,
         gitHubLink: 'https://github.com/weomweom/memory-game'
+    },
+    {
+        title: 'Currnecy converter',
+        skills: 'React, JS, HTML, CSS, API',
+        img: projects04,
+        imgBig: projects04Big,
+        gitHubLink: 'https://github.com/weomweom/currency-converter'
     }
 ];
 
